a075d5d65636f31f773f811d89a758a96b63e91a
W33D <sky.t4y@gmail.com>
2020-06-16 18:22:59 +0800

    Modified constant to allow flexible paths

--- goenv/constants.py
+++ goenv/constants.py
@@ -6,9 +6,9 @@ XDG_CACHE_HOME = os.environ.get('PWD', False) or \
                  os.path.join(os.environ['HOME'], ".cache")
 XDG_CONFIG_HOME = os.environ.get('PWD') or \
                   os.path.join(os.environ['HOME'], ".config")
-GOENV_CACHE_HOME = os.path.join(XDG_CACHE_HOME, ".goenv")
-GOENV_CONFIG_HOME = os.path.join(XDG_CONFIG_HOME, ".goenv",)
-GOLANG_DISTRIBUTIONS_DIR = os.path.join(GOENV_CONFIG_HOME, "dists")
+GOENV_CACHE_HOME = "downloads"
+GOENV_CONFIG_HOME = ".goenv"
+GOLANG_DISTRIBUTIONS_DIR = "dists"
 
 DOWNLOAD_HOSTNAME = "storage.googleapis.com"
 DOWNLOAD_PATH = "/golang/{filename}"
